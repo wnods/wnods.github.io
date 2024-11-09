7840d8854dcb240b6bf5bd4686a8f9d284a3c204
wnods <wilsonicolai@gmail.com>
2024-11-09 01:49:22 -0300

    nav-bar responsvie and toggle menu

--- src/script/falcon-theme-toggle.js
+++ src/script/falcon-theme-toggle.js
@@ -1,21 +1,4 @@
-
-const themeToggleButton = document.getElementById('theme-toggle');
-
-
-function updateButtonIcon() {
-    if (document.body.classList.contains('light-theme')) {
-        themeToggleButton.textContent = '🌞'; 
-    } else {
-        themeToggleButton.textContent = '🌙'; 
-    }
-}
-
-
-themeToggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-    updateButtonIcon();
-});
-
-
-updateButtonIcon();
+function toggleMenu() {
+    const menu = document.getElementById('navbar');
+    menu.classList.toggle('active');
+  }
